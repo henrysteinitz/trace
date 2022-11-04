b3e75de15280f8925034bc74eaaefb7a0158446d
Henry <hjsteinitz@gmail.com>
2022-11-04 17:58:47 -0400

    Add AssociativeAttention module

---
+++ architecture/attention/associative_test.py
@@ -0,0 +1,31 @@
+import torch
+from torch.testing import assert_close
+import unittest
+
+from associative import AssociativeAttention
+
+
+class TestScaledDotProductAttention(unittest.TestCase):
+
+	def test_single_attention(self):
+		module = AssociativeAttention()
+		query = torch.Tensor([[3, 3, 4, 4, 0, 0, 0]])
+		memory = torch.Tensor([[
+			[3, 3, 4, 4, 7, 7, 7],
+			[1, 2, 1, 0, 0, 2, 2],
+			[10, 0, 1, 0, 4, -1, 1],
+		]])
+
+		# Beacuse the query most closely matches the first row 
+		# in memory the module should approximatelty complete the first 
+		# row.
+		assert_close(
+			module(query, memory), 
+			torch.tensor([[[3.0, 3.0, 4.0, 4.0, 7.0, 7.0, 7.0]]]), 
+			atol=.01, 
+			rtol=0
+		)
+
+
+if __name__ == '__main__':
+    unittest.main()
