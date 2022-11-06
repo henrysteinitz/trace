c4ba3f5309d5680215bab0746b81579de7c1dcb7
Henry <hjsteinitz@gmail.com>
2022-11-06 10:56:03 -0500

    Add hypernetwork forward computation test

--- architecture/hypernetwork/dense_test.py
+++ architecture/hypernetwork/dense_test.py
@@ -15,5 +15,23 @@ class TestDenseHypernetwork(unittest.TestCase):
 		self.assertEqual(model(x).size(), torch.Size([1, 3]))
 
 
+	def test_forward_value(self):
+		base_model = nn.Linear(in_features=1, out_features=1, bias=True)
+		model = DenseHypernetwork(base_model, input_shape=[1])
+		
+		state = model.state_dict()
+		state["named_submodels.weight.weight"] = torch.Tensor([[1.0]])
+		state["named_submodels.weight.bias"] = torch.Tensor([2.0])
+		state["named_submodels.bias.weight"] = torch.Tensor([[2.0]])
+		state["named_submodels.bias.bias"] = torch.Tensor([2.0])
+		model.load_state_dict(state)
+
+		# When X = 1, the hypernetwork's weight submodel will ouput 1(1) + 2 = 3.
+		# Similarly the bias submodel will output 2(1) + 2 = 4. The output of 
+		# the base model should then be 3(1) + 4 = 7. 
+		x = torch.Tensor([[1.0]])
+		self.assertEqual(model(x), torch.Tensor([[7.0]]))
+
+
 if __name__ == '__main__':
     unittest.main()
