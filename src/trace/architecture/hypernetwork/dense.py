b4d6e7e0af684c4db6ce7078eaede168305c6c68
Henry <hjsteinitz@gmail.com>
2022-11-07 10:34:20 -0500

    Modify DenseHypernetwork class to support iterated hypernetworks with nested ModuleDicts

--- src/trace/architecture/hypernetwork/dense.py
+++ src/trace/architecture/hypernetwork/dense.py
@@ -13,7 +13,7 @@ class DenseHypernetwork(nn.Module):
 
 		for name, parameter in self.base_model.named_parameters():
 			# Module names cannot contain '.'.
-			modified_name = name.replace(".", "_")
+			modified_name = name.replace(".", "/")
 			self.named_submodels[modified_name] = nn.Linear(
 				in_features=in_features, 
 				out_features=numpy.prod(parameter.size()), 
@@ -26,13 +26,20 @@ class DenseHypernetwork(nn.Module):
 	# outputs per batch or compute each batch example individually.
 	def forward(self, x):
 		x = torch.flatten(x, start_dim=1, end_dim=-1)
-
 		for name in self.named_submodels:
 			# Access nested module.
-			module_path = name.split("_")
+			module_path = name.split("/")
+
 			base_model = self.base_model
+			current_path = ""
 			for i in range(len(module_path) - 1):
-				base_model = base_model._modules[module_path[i]]
+				current_path += module_path[i]
+				if current_path not in base_model._modules:
+					current_path += "/"
+					continue
+
+				base_model = base_model._modules[current_path]
+				current_path = ""
 
 			# Reshape submodel output.
 			submodel_out = self.named_submodels[name](x)
@@ -41,7 +48,7 @@ class DenseHypernetwork(nn.Module):
 			# Set base model weights to submodel output. We need to attach the tensor directly
 			# to base_model.__dict__ because Module.__setattr__ enforces that existing parameters 
 			# remain parameters. We cannot contruct a parameter from our tensor because that will
-			# erase its computational history and break future gradient computations. 
+			# erase its computational history and break future gradient computations.
 			base_model.__dict__[module_path[-1]] = submodel_out
 
 		return self.base_model(x)
