50805acc790a9dc3c70642ee2f80e58fa32b4610
Henry <hjsteinitz@gmail.com>
2022-11-06 20:10:06 -0500

    Add quadractic regression test for 2 layer DenseHypernetwork

--- architecture/hypernetwork/dense_test.py
+++ architecture/hypernetwork/dense_test.py
@@ -22,6 +22,21 @@ class SyntheticLinearRegressionDataset(Dataset):
         return self.X[idx], self.Y[idx]
 
 
+class SyntheticQuadraticRegressionDataset(Dataset):
+
+    def __init__(self, A, b, size):
+        self.X = [torch.rand_like(b) for _ in range(size)]
+        self.Y = [torch.matmul(torch.matmul(A, x), x) + torch.matmul(A, x) + b for x in self.X]
+
+
+    def __len__(self):
+        return len(self.X)
+
+
+    def __getitem__(self, idx):
+        return self.X[idx], self.Y[idx]
+
+
 class TestDenseHypernetwork(unittest.TestCase):
 
 	def test_forward_shape(self):
@@ -80,6 +95,38 @@ class TestDenseHypernetwork(unittest.TestCase):
 		# produce [10, 15].
 		assert_close(model(torch.Tensor([[1., 1.]])), torch.Tensor([[10., 15.]]), atol=.001, rtol=0)
 
+
+	def test_quadradic_regression(self):
+		# A hypernetwork does not need nonlinearities to compute nonlinear functions. In fact,
+		# a deep linear hypernetwork with n layers can compute n-degree polynomials.
+		base_model = nn.Sequential(
+			nn.Linear(in_features=2, out_features=2, bias=True),
+			nn.Linear(in_features=2, out_features=2, bias=True)
+		)
+		model = DenseHypernetwork(base_model, input_shape=[2])
+
+		A = torch.Tensor([
+			[1.0, 2.0],
+			[-1.0, -1.0]
+		])
+		b = torch.Tensor([3.0, 4.0])
+
+		# TODO: Move training loop into helper function.
+		dataset = SyntheticQuadraticRegressionDataset(A=A, b=b, size=10000)
+		data = DataLoader(dataset, batch_size=1, shuffle=True)
+		optimizer = optim.SGD(model.parameters(), lr=.03, momentum=0.9)
+		loss_fn = nn.HuberLoss()
+		for _ in range(7):
+			for X, Y in data:
+				optimizer.zero_grad()
+				Y_prime = model(X)
+				loss = loss_fn(Y_prime, Y)
+				loss.backward()
+				optimizer.step()
+
+		# (Ax)x + Ax + b for x = [1, 1] yields [7, 3]. The tolerance is increased to 
+		# reduce the total number of epochs and speed up the test.
+		assert_close(model(torch.Tensor([[1., 1.]])), torch.Tensor([[7., 3.]]), atol=.01, rtol=0.)
 
 
 if __name__ == '__main__':
